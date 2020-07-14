1b10400a51417323790a5c702436154a4c59789a
AronDemian <AronManning5@gmail.com>
2020-07-14 03:42:13 +0200

    WebRequest: Add support for $wgExtraRouterPaths
    Sample LocalSettings config:
    ```
    $wgExtraRouterPaths = [
    	'/h/$1' => [ 'action' => 'history', 'title' => '$1' ],
    	'/e/$1' => [ 'action' => 'edit', 'title' => '$1' ],
    	'/special/$1' => [ 'title' => 'Special:$1' ],
    	'/s/$1' => [ 'title' => 'Special:$1' ],
    	'/talk/$1' => [ 'title' => 'Talk:$1' ],
    	'/t/$1' => [ 'title' => 'Talk:$1' ],
    	'/user/$1' => [ 'title' => 'User:$1' ],
    	'/u/$1' => [ 'title' => 'User:$1' ],
    	'/usertalk/$1' => [ 'title' => 'User_talk:$1' ],
    	'/ut/$1' => [ 'title' => 'User_talk:$1' ],
    	'/meta/$1' => [ 'title' => 'Meta:$1' ],
    	'/m/$1' => [ 'title' => 'Meta:$1' ],
    	'/metatalk/$1' => [ 'title' => 'Meta_talk:$1' ],
    	'/mt/$1' => [ 'title' => 'Meta_talk:$1' ],
    ];
    ```
    
    Change-Id: I924238338c3ecbb0f085f2c3c7ad0b53084aa297

--- includes/WebRequest.php
+++ includes/WebRequest.php
@@ -185,6 +185,11 @@ class WebRequest {
 				$router->add( $articlePaths, [ 'action' => '$key' ] );
 			}
 
+			global $wgExtraRouterPaths;
+			foreach ( ( $wgExtraRouterPaths ?? [] ) as $pathPattern => $params ) {
+				$router->add( $pathPattern, $params );
+			}
+
 			global $wgVariantArticlePath;
 			if ( $wgVariantArticlePath ) {
 				$router->validateRoute( $wgVariantArticlePath, 'wgVariantArticlePath' );
