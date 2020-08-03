401f133a6e0c49d1f2e8d64ff2bd96518f15662e
AronDemian <AronManning5@gmail.com>
2020-08-04 01:35:58 +0200

    sharewidget: Split into functions, reduce indent
    Change-Id: I16f63f3f16a4d69574c6cf2b9c14edc4ae04dda4

--- resources/src/mediawiki.page.sharewidget/sharewidget.js
+++ resources/src/mediawiki.page.sharewidget/sharewidget.js
@@ -3,66 +3,73 @@
  */
 ( function () {
 	mw.hook( 'wikipage.content' ).add( function ( $content ) {
-		$content.find( '.mw-editsection-share[data-mw-share-section]' ).each( function () {
-			var shareLink = this, $shareLink = $( this ), popup,
-				linkWikitext = mw.Title.newFromText(
-					mw.config.get( 'wgPageName' )
-				).getPrefixedText() + '#' + $shareLink.attr( 'data-mw-share-section' );
-
-			// Already initialized
-			if ( $shareLink.data( 'mw-share-init' ) ) {
-				return;
-			}
-
-			$shareLink.data( 'mw-share-init', true ).on( 'click', function ( e ) {
-				var modules = [
-					'mediawiki.widgets', 'mediawiki.editfont.styles', 'mediawiki.jqueryMsg'
-				];
-				e.preventDefault();
-
-				if ( !popup ) {
-					mw.loader.using( modules, function () {
-						var copiableURL = new mw.widgets.CopyTextLayout( {
-								label: mw.msg( 'share-url' ),
-								copyText: shareLink.href,
-								align: 'top',
-								textInput: {
-									classes: [ 'mw-editfont-' + mw.user.options.get( 'editfont' ) ]
-								}
-							} ),
-							copiableWikitext = new mw.widgets.CopyTextLayout( {
-								label: mw.msg( 'share-wikitext' ),
-								copyText: linkWikitext,
-								align: 'top',
-								textInput: {
-									classes: [ 'mw-editfont-' + mw.user.options.get( 'editfont' ) ]
-								}
-							} );
-
-						popup = new OO.ui.PopupWidget( {
-							$floatableContainer: $shareLink,
-							$content: $( '<div>' )
-								.append(
-									$( '<h4>' ).addClass( 'mw-editsection-share-popup-title' )
-										.text( mw.msg( 'share-popup-title' ) ),
-									copiableURL.$element,
-									copiableWikitext.$element
-								),
-							classes: [ 'mw-editsection-share' ],
-							align: 'forwards',
-							autoClose: true,
-							padded: true,
-							width: 380
-						} );
-
-						OO.ui.getDefaultOverlay().append( popup.$element );
-
-						popup.toggle();
-					} );
-				} else {
-					popup.toggle();
-				}
-			} );
-		} );
+		$content.find( '.mw-editsection-share[data-mw-share-section]' ).each( initPopup );
 	} );
+
+	function initPopup() {
+		var $shareLink = $( this ), popup;
+
+		// Already initialized
+		if ( $shareLink.data( 'mw-share-init' ) ) {
+			return;
+		}
+
+		$shareLink.data( 'mw-share-init', true ).on( 'click', function ( e ) {
+			var modules = [
+				'mediawiki.widgets', 'mediawiki.editfont.styles', 'mediawiki.jqueryMsg'
+			];
+			e.preventDefault();
+
+			if ( !popup ) {
+				mw.loader.using( modules, function() {
+					popup = createPopup( $shareLink );
+				} );
+			} else {
+				popup.toggle();
+			}
+		} );
+	}
+
+	function createPopup( $shareLink ) {
+		var linkWikitext = mw.Title.newFromText( mw.config.get( 'wgPageName' ) )
+			.getPrefixedText() + '#' + $shareLink.attr( 'data-mw-share-section' );
+		var copiableURL = new mw.widgets.CopyTextLayout( {
+			label: mw.msg( 'share-url' ),
+			copyText: $shareLink[0].href,
+			align: 'top',
+			textInput: {
+				classes: [ 'mw-editfont-' + mw.user.options.get( 'editfont' ) ]
+			}
+		} ),
+		copiableWikitext = new mw.widgets.CopyTextLayout( {
+			label: mw.msg( 'share-wikitext' ),
+			copyText: linkWikitext,
+			align: 'top',
+			textInput: {
+				classes: [ 'mw-editfont-' + mw.user.options.get( 'editfont' ) ]
+			}
+		} );
+
+		var popup = new OO.ui.PopupWidget( {
+			$floatableContainer: $shareLink,
+			$content: $( '<div>' )
+				.append(
+					$( '<h4>' ).addClass( 'mw-editsection-share-popup-title' )
+						.text( mw.msg( 'share-popup-title' ) ),
+					copiableURL.$element,
+					copiableWikitext.$element
+				),
+			classes: [ 'mw-editsection-share' ],
+			align: 'forwards',
+			autoClose: true,
+			padded: true,
+			width: 380
+		} );
+
+		OO.ui.getDefaultOverlay().append( popup.$element );
+
+		popup.toggle();
+		return popup;
+	}
+
 }() );
