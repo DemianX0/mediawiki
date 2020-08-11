536378a4c12c924f42dba2d1440b25f843a595ff
AronDemian <AronManning5@gmail.com>
2020-08-12 01:06:53 +0200

    [cleanup] NamespaceInfo: code format and speedup
    - No need to use array_keys().
    
    Change-Id: I9804d8a96040a7cf09d912c9f6c82a0c56326583

--- includes/title/NamespaceInfo.php
+++ includes/title/NamespaceInfo.php
@@ -253,11 +253,7 @@ class NamespaceInfo {
 			return false;
 		}
 
-		if ( $target->getNamespace() < NS_MAIN ) {
-			return false;
-		}
-
-		return true;
+		return ( $target->getNamespace() >= NS_MAIN );
 	}
 
 	/**
@@ -431,7 +427,7 @@ class NamespaceInfo {
 	public function getValidNamespaces() {
 		if ( $this->validNamespaces === null ) {
 			$this->validNamespaces = [];
-			foreach ( array_keys( $this->getCanonicalNamespaces() ) as $ns ) {
+			foreach ( $this->getCanonicalNamespaces() as $ns => $name ) {
 				if ( $ns >= 0 ) {
 					$this->validNamespaces[] = $ns;
 				}
